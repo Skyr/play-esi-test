0d8d8cbbf060812683515dca5b219b4ab386c851
Stefan Schlott <stefan@ploing.de>
2015-06-08 09:39:12 +0200

    Get+embed snippet via WS

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,15 +1,24 @@
 package controllers
 
 import java.util.Date
+import javax.inject.Inject
 
 import play.api._
+import play.api.libs.ws.WSClient
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
 
-class Application extends Controller {
 
-  def index = Action { implicit request =>
+class Application @Inject() (ws: WSClient) extends Controller {
+
+  def index = Action.async { implicit request =>
     val sessionInfo = request.session.get("info").getOrElse("")
-    Ok(views.html.index(sessionInfo))
+    // val headers = request.cookies.map(c => "Cookie" -> c.value)
+    val headers = request.headers.headers
+    val snippet = ws.url(routes.Application.esiSnippet().absoluteURL()).withHeaders(headers:_*)
+    snippet.get().map { resp =>
+      Ok(views.html.index(sessionInfo, resp.body))
+    }
   }
 
   def setCookie = Action {
